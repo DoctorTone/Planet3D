50a60db7fc0312c50df21f1572bf76e6bf35152e
DoctorTone <tony@drt-software.com>
2024-11-19 00:26:07 +0000

    Start to add content pages

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,7 +1,52 @@
+import Container from "@mui/material/Container";
 import Header from "../components/Header";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+import Typography from "@mui/material/Typography";
 
 const MainPage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Box sx={{ mt: 5 }}>
+          <Grid container spacing={2} sx={{ mt: 3, padding: "30px" }}>
+            <Grid
+              size={{ xs: 12, md: 6 }}
+              sx={{
+                background: "#404040",
+                borderRadius: "20px",
+                textAlign: "center",
+              }}
+            >
+              <Typography variant="h5" color="#386fc7">
+                Web Frameworks
+              </Typography>
+              <Typography variant="h6" color="grey">
+                Frameworks and libraries for building 3D websites/applications
+                for the web.
+              </Typography>
+            </Grid>
+            <Grid
+              size={{ xs: 12, md: 6 }}
+              sx={{
+                background: "#404040",
+                borderRadius: "20px",
+                textAlign: "center",
+              }}
+            >
+              <Typography variant="h5" color="#386fc7">
+                3D Engines
+              </Typography>
+              <Typography variant="h6" color="grey">
+                Applications for building 3D content on the desktop.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default MainPage;
--- src/UI/Text.tsx
+++ src/UI/Text.tsx
@@ -5,7 +5,7 @@ const Text = () => {
   return (
     <>
       <div id="titleTop" className="panel plus-jakarta-sans-main">
-        <Typography variant="h3" sx={{ color: "#c0e04c", textAlign: "center" }}>
+        <Typography variant="h3" sx={{ color: "yellow", textAlign: "center" }}>
           Everything 3D
         </Typography>
       </div>
