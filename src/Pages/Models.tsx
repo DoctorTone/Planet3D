809bfb8d49101fe123e4b96f498a7187429b70ca
DoctorTone <tony@drt-software.com>
2024-11-19 11:44:11 +0000

    Added more content

--- src/Pages/Models.tsx
+++ src/Pages/Models.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+import Header from "../components/Header";
+import Item from "../components/Item";
+import Footer from "../components/Footer";
 
 const Models = () => {
-  return <div>Models</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Box sx={{ mt: 5 }}>
+          <Grid container spacing={4} sx={{ mt: 5 }}>
+            <Item
+              title="Pmndrs Market"
+              body="A collection of low poly free 3D models."
+              link="https://market.pmnd.rs/"
+              external={true}
+            />
+            <Item
+              title="Sketchfab"
+              body="A huge collection of models in many formats and price ranges."
+              link="https://sketchfab.com/feed"
+              external={true}
+            />
+            <Item
+              title="Turbosquid"
+              body="One of the biggest collection of models in many formats and price ranges."
+              link="https://www.turbosquid.com/"
+              external={true}
+            />
+          </Grid>
+        </Box>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default Models;
--- src/Pages/Textures.tsx
+++ src/Pages/Textures.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+import Header from "../components/Header";
+import Item from "../components/Item";
+import Footer from "../components/Footer";
 
 const Textures = () => {
-  return <div>Textures</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Box sx={{ mt: 5 }}>
+          <Grid container spacing={4} sx={{ mt: 5 }}>
+            <Item
+              title="Poly Haven"
+              body="A huge 3D asset library, including seamless textures and HDRI's."
+              link="https://polyhaven.com/"
+              external={true}
+            />
+            <Item
+              title="textures.com"
+              body="A large collection of textures, organised by topic."
+              link="https://www.textures.com/"
+              external={true}
+            />
+            <Item
+              title="ambientCG"
+              body="A collection of seamless textures and materials."
+              link="https://ambientcg.com/"
+              external={true}
+            />
+          </Grid>
+        </Box>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default Textures;
