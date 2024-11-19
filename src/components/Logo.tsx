0b85f2449e47508a8bfcf6507cbc89678871fd19
DoctorTone <tony@drt-software.com>
2024-11-19 12:30:28 +0000

    Fix logo position

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <div id="logo" className="panel">
+    <div id="logo" className="panel centred">
       <img className="circular w-60" src="./images/planet3d.jpg" />
     </div>
   );
